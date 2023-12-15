f919897b92a686eabd8f3b0f5f99f7b75d830b78
Sosa Max Planck <s.sosa@live.fr>
2023-12-15 15:56:32 +0100

    run gui

--- gui/pannel_model_run.py
+++ gui/pannel_model_run.py
@@ -1,5 +1,31 @@
+#%%
 import flet as ft
 import pandas as pd
 from gui.utils import *
+from bayesian.distribution import *
+from code.model_write import *
+from code.model_fit import *
+from code.model_diagnostic import *
 
+# Model definition page ----------------------------------
+class Run(ft.UserControl):
+    # Link page attributes
+    def __init__(self, page: ft.Page):
+        super().__init__()
+        self.page = page
+        self.page.df = page.df  
+
+        
+    #Build must be implemented which will return the UserTextField along with
+    #The Add Button
+    def build(self):
+        
+        #Text Field to type our task in
+        self.textField = ft.TextField(label="Write equation", multiline=True, width=self.page.window_width*0.70)  
+
+        taskRow=ft.ResponsiveRow([
+                    ft.Column(col={"sm": 11, "md": 11, "xl": 9},
+                     controls=[ft.Row(controls=[self.textField,])])])
+
+        return taskRow
 
