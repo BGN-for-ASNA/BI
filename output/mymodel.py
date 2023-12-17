ccfbba36446473927e7def55d882d9fa1a54779e
Sebastian Sosa <s.sosa@live.fr>
2023-12-17 10:28:25 +0100

    categorical variables

--- output/mymodel.py
+++ output/mymodel.py
@@ -1,6 +1,7 @@
 # Import dependencies ----------------------------
 import tensorflow as tf
 import tensorflow_probability as tfp
+import pandas as pd
 tfd = tfp.distributions
 from code.model_fit import *
 from code.model_diagnostic import *
@@ -10,11 +11,16 @@ physical_devices = tf.config.experimental.list_physical_devices('GPU')
 if len(physical_devices) > 0:
 	tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
+# Import data (with modification if maded) ----------------------------
+d = pd.read_csv('output/mydf.csv', sep = ',')
+cat= d.cat
+height= d.height
+
 # Model ----------------------------
 m = tfd.JointDistributionNamed(dict(
-	s = tfd.Sample(tfd.Exponential(1), sample_shape=1),
-	alpha = tfd.Sample(tfd.Normal(0, 1), sample_shape=1),
+	sigma = tfd.Sample(tfd.Uniform(0, 50), sample_shape=1),
+	alpha = tfd.Sample(tfd.Normal(178, 20), sample_shape=1),
 	beta = tfd.Sample(tfd.Normal(0, 1), sample_shape=1),
 
-	y = lambda alpha,beta,s: tfd.Independent(tfd.Normal(alpha+beta, s), reinterpreted_batch_ndims=1),
+	height = lambda alpha,beta,sigma: tfd.Independent(tfd.Normal(alpha+beta*cat, sigma), reinterpreted_batch_ndims=1),
 ))
--- test gui.py
+++ test gui.py
@@ -4,6 +4,7 @@ from gui.gui import main
 from code.model_write import *
 from code.model_fit import *
 from code.model_diagnostic import *
+from code.data_manip import OHE
 ft.app(target=main)
 
 
