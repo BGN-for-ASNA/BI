6c57834ef6b02992fd50c0e00cb384dff09724a8
Sebastian Sosa <s.sosa@live.fr>
2024-01-20 11:25:42 +0100

    test with classes

--- code/data_manip.py
+++ code/data_manip.py
@@ -1,33 +1,6 @@
 #%%
 import pandas as pd
 import numpy as np
-def OHE(d, cols = 'all'):
-    if cols == 'all':
-        colCat = list(d.select_dtypes(['object']).columns)    
-        OHE = pd.get_dummies(d, columns=colCat, dtype=int)
-    else:
-        OHE = pd.get_dummies(d, columns=cols, dtype=int)
-    
-    OHE.columns = OHE.columns.str.replace('.', '_')
-    OHE.columns = OHE.columns.str.replace(' ', '_')
-    return OHE
-
-def index(d, cols = 'all'):
-    index = d
-    if cols == 'all':
-        colCat = list(d.select_dtypes(['object']).columns)    
-        for a in range(len(colCat)):
-            d["index_"+ colCat[a]] =  d.loc[:,colCat[a]].astype("category").cat.codes
-            d["index_"+ colCat[a]] = d["index_"+ colCat[a]].astype(np.int64)
-    else:
-        for a in range(len(cols)):
-            d["index_"+ cols[a]] =  d.loc[:,cols[a]].astype("category").cat.codes
-            d["index_"+ cols[a]] = d["index_"+ cols[a]].astype(np.int64)
-
-    d.columns = d.columns.str.replace('.', '_')
-    d.columns = d.columns.str.replace(' ', '_')
-    return d
-
 #%%
 class data():
     # Import data----------------------------
