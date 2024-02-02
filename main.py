af94c167ba9df62b2caaf1ad6b3501c8cda59986
Sebastian Sosa <s.sosa@live.fr>
2024-02-02 10:55:37 +0100

    Fit and diag ready (remain plot predictions)

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-#from code.model_diagnostic import *
-#from code.model_fit import *
-from code.class_write import*
-from code.data_manip import data
+
+from code.data import*
+from code.build import*
+from code.fit import*
+from code.diagnostic import*
 import pandas as pd
 import numpy as np  
 import tensorflow as tf
@@ -22,22 +23,14 @@ tf_classes = get_distribution_classes()
 def exportTFD(tf_classes):
     for key in tf_classes.keys():
         globals()[key] = tf_classes[key]
-
 exportTFD(tf_classes)
 
-def joineDis(tensor, text, df):
-    df = df
-    print(text)
-    for key in text.keys():
-        tensor[key] =  eval(f"{text[key]}")
-    tensor = tfd.JointDistributionNamed(tensor)
-    return tensor
-
-class model(data, define, write):
+class model(data, define, write, fit, diagnostic):
     def __init__(self, 
                  formula = None, 
                  df = None,
-                 float = 16,
+                 float = 32,  
+                 gpu = True,               
                  **kwargs):      
         self.f = formula
         if df is None:
@@ -51,38 +44,70 @@ class model(data, define, write):
         self.model_path = 'output/mymodel.py'
         self.df_path = 'output/mydf.csv'
         self.data_modification = {}
-        self.float = float
-        
+        if float == 16:
+            self.float = tf.float16
+            self.int = tf.int16
+        if float == 32:
+            self.float = tf.float32
+            self.int = tf.int32
+        if float == 64:
+            self.float = tf.float64
+            self.int = tf.int64
+
         # GPU configuration ----------------------------
-        physical_devices = tf.config.experimental.list_physical_devices('GPU')
-        if len(physical_devices) > 0:
-        	tf.config.experimental.set_memory_growth(physical_devices[0], True)
-        
-        # If user setup directly a formula
+        if gpu:
+            physical_devices = tf.config.experimental.list_physical_devices('GPU')
+            if len(physical_devices) > 0:
+                tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
         if formula is not None:
             self.formula(self.f)
             self.build_model()
-        
+
     def build_model(self):
+        # Gather formula input informations
         self.get_var()
         self.get_priors_names()
+        self.get_model_type()
         self.get_mains_info() 
         
-        self.output_path = 'output/mydf.csv'
-        self.df.to_csv(self.output_path, index=False)
+        #self.output_path = 'output/mydf.csv'
+        #self.df.to_csv(self.output_path, index=False)
 
+        # Formula input to tensorflow probability model
         self.tensor_prior()
         self.write_main_text()
         for key in self.main_text.keys():
             self.tensor[key] = self.create_function_from_string(func_str = self.main_text[key], name = key)
+
+        self.tensor_dict = self.tensor
         self.tensor = tfd.JointDistributionNamed(self.tensor)
-        #new_tensor = joineDis(self.tensor, self.main_text, self.df)
-        #self.tensor = new_tensor
-        #print("Model builded")
-        #return new_tensor
         
     def sample(self, *args, **kwargs):
         self.samples = self.tensor.sample(*args, **kwargs)
         return self.samples
+    
+    def log_prob(self, *args, **kwargs):
+        self.prob = self.tensor.log_prob(*args, **kwargs)
+        return self.prob
+    
+    def fit(self, observed_data,
+            parallel_iterations=1,
+            num_results=2000,
+            num_burnin_steps=500,
+            step_size=0.065,
+            num_leapfrog_steps=5,
+            num_adaptation_steps=400,
+            num_chains=4):
         
-
+        self.posterior, self.trace, self.sample_stats = self.run_model(observed_data,
+                       parallel_iterations=parallel_iterations,
+                       num_results=num_results,
+                       num_burnin_steps=num_burnin_steps,
+                       step_size=step_size,
+                       num_leapfrog_steps=num_leapfrog_steps,
+                       num_adaptation_steps=num_adaptation_steps,
+                       num_chains=num_chains)
+        #self.posterior = posterior
+        #self.trace = trace
+        #self.sample_stats = sample_stats
