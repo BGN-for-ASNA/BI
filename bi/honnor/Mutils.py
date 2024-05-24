884172c227451fd95404698468e1bdcd691880bf
Sebastian Sosa <sosa@ECOD052.eva.mpg.de>
2024-05-24 13:51:00 +0200

    Gaussian kernels added

--- bi/honnor/Mutils.py
+++ bi/honnor/Mutils.py
@@ -43,3 +43,80 @@ def cov_GPL2(x, sq_eta, sq_rho, sq_sigma):
     K = sq_eta * jnp.exp(-sq_rho * jnp.square(x))
     K = K.at[jnp.diag_indices(N)].add(sq_sigma)
     return K
+
+
+
+@jit
+def mat_distance(array):
+    return jnp.abs(array[:, None] - array[None, :])
+
+
+@jit
+def sq_exp_kernel(m, sq_alpha=0.5, sq_rho=0.1, delta=0):
+    """Squared Exponential Kernel.
+
+    The SE kernel is a widely used kernel in Gaussian processes (GPs) and support vector machines (SVMs). It has some desirable properties, such as universality and infinite differentiability. This function computes the covariance matrix using the squared exponential kernel.
+
+    Args:
+        m (array): Input array representing the absolute distances between data points.
+        sq_alpha (float, optional): Scale parameter of the squared exponential kernel. Defaults to 0.5.
+        sq_rho (float, optional): Length-scale parameter of the squared exponential kernel. Defaults to 0.1.
+        delta (int, optional): Delta value to be added to the diagonal of the covariance matrix. Defaults to 0.
+
+    Returns:
+        tuple: A tuple containing:
+            - K (array): The covariance matrix computed using the squared exponential kernel.
+            - cov (array): A masked covariance matrix with the upper triangular part set to zero.
+    """
+    # Get the number of data points
+    N = m.shape[0]
+    
+    # Compute the kernel matrix using the squared exponential kernel
+    K = sq_alpha * jnp.exp(-sq_rho *  jnp.square(m))
+    
+    # Set the diagonal elements of the kernel matrix
+    K = K.at[jnp.diag_indices(N)].set(sq_alpha + delta)
+    
+    # Create a mask for the upper triangular part of the covariance matrix
+    mask = jnp.triu(jnp.ones_like(K, dtype=bool))
+    
+    # Apply the mask to set the upper triangular part of the covariance matrix to zero
+    cov = jnp.where(mask, K, 0)
+    
+    return K, cov
+
+@jit
+def periodic_kernel(m, sigma=1, length_scale=1.0, period=1.0):
+    """Periodic Kernel.
+
+    The periodic kernel is often used in Gaussian processes (GPs) for modeling functions with periodic behavior.
+
+    Args:
+        m (array): Input array representing the absolute distances between data points.
+        sigma (float, optional): Scale parameter of the kernel. Defaults to 1.0.
+        length_scale (float, optional): Length scale parameter of the kernel. Defaults to 1.0.
+        period (float, optional): Period parameter of the kernel. Defaults to 1.0.
+
+    Returns:
+        array: The covariance matrix computed using the periodic kernel.
+    """    
+    # Compute the kernel matrix using the squared exponential kernel
+    return sigma**2 * jnp.exp(-2*jnp.sin(jnp.pi * m / period)**2 / length_scale**2) 
+
+@jit
+def local_periodic_kernel(m, sigma=1, length_scale=1.0, period=1.0):
+    """Locally Periodic Kernel
+
+    A SE kernel times a periodic results in functions which are periodic, but which can slowly vary over time.
+
+    Args:
+        m (array): Input array representing the absolute distances between data points.
+        sigma (float, optional): Scale parameter of the kernel. Defaults to 1.0.
+        length_scale (float, optional): Length scale parameter of the kernel. Defaults to 1.0.
+        period (float, optional): Period parameter of the kernel. Defaults to 1.0.
+
+    Returns:
+        array: The covariance matrix computed using the periodic kernel.
+    """    
+    # Compute the kernel matrix using the squared exponential kernel
+    return sigma**2 * jnp.exp(-2*jnp.sin(jnp.pi * m / period)**2 / length_scale**2)  * jnp.exp(-(m**2/ 2*length_scale**2))
