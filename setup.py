f956a344576ac68c9442c01d75dfdf9f0c64f716
Sosa MPI <s.sosa@live.fr>
2024-10-17 08:19:15 +0200

    removing none used data of rethinking package

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ LONG_DESCRIPTION = 'Run Bayesian inferences models'
 # Setting up
 setup(
        # the name must match the folder name 'verysimplemodule'
-        name="BY", 
+        name="BI", 
         version=VERSION,
         author="Sebastian Sosa",
         author_email="<s.sosa@live.fr>",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(),
-        install_requires=[], # add any additional packages that 
+        install_requires=[numpyro, tensorflow_probability], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
-        
+        package_data={'my_pkg/resources/nltk_data/*'}
         keywords=['python', 'first package'],
         classifiers= [
             "Development Status :: 3 - Alpha",
