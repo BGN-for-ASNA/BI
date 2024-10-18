232df75c896b3196c2839033433ab329cc8ce23e
Sosa MPI <s.sosa@live.fr>
2024-10-18 16:25:21 +0200

    folder structure modify over and working + code block theme

--- bi/network/util.py
+++ bi/network/util.py
@@ -1,7 +1,7 @@
 import inspect
 from numpyro import sample as lk
 from numpyro import deterministic
-from unified_dists import UnifiedDist as dist
+from utils.unified_dists import UnifiedDist as dist
 from Mutils import Mgaussian
 from Mutils import factors
 import jax 
