a27bf1b38c241d191684870ab563f23e0f5770ec
Sosa MPI <s.sosa@live.fr>
2024-11-06 11:01:04 +0100

    sample model over

--- bi/network/net.py
+++ bi/network/net.py
@@ -1,9 +1,9 @@
 from network.metrics import met
-from network.util import manip
+from network.util import array_manip 
 from network.model_effects import Neteffect 
 import jax.numpy as jnp
 
-class net(met, Neteffect,manip):
+class net(met, Neteffect, array_manip):
     def __init__(self, *args, **kwargs):
         # Call super() without specifying the class name in a multiple inheritance context
         super().__init__(*args, **kwargs)
