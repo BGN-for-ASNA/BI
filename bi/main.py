88648c7f61fe03b289f1a5dee8af3e0f5fe8aa9f
SebastianSosa <s.sosa@live.fr>
2024-12-17 11:48:59 +0100

    diag class working

--- bi/main.py
+++ bi/main.py
@@ -20,6 +20,7 @@ from network.net import net
 from mcmc.Link import link
 from mcmc.Model import model
 from mcmc.Samplers import samplers
+from diagnostic.Diag import diag
 
 from utils.unified_dists import tfpLight as dist
 
@@ -31,7 +32,7 @@ tfb = tfp.bijectors
 import inspect
 import re
 
-class bi(manip, dist, gaussian, factors, net, link, samplers ):
+class bi(manip, dist, gaussian, factors, net, link, samplers, diag):
     def __init__(self, platform='cpu', cores=None, deallocate = False, dtype = jnp.float32):
         setup(platform, cores, deallocate) 
         jax.config.update("jax_enable_x64", True)
@@ -59,166 +60,14 @@ class bi(manip, dist, gaussian, factors, net, link, samplers ):
     class samplers(samplers):
         pass
 
-    # Get posteriors ----------------------------------------------------------------------------
-    def trace_to_az(self, sample_stats_name=['target_log_prob','log_accept_ratio','has_divergence','energy']):
+    # MCMC samplers --------------------------
+    class samplers(samplers):
+        pass
 
-        var_names= list(self.model_info.keys())
-        sample_stats = {k:jnp.transpose(v) for k, v in zip(sample_stats_name, self.sample_stats)}
-        trace = {}
-        #First dim is the number of chains
-        #Second dim is the number of sampling
-        #The rest is the shape of the object
-        for name, samp in zip(var_names, self.posterior):
-            print(samp.shape)
-            trace[name] = samp
-        self.trace = az.from_dict(posterior=trace, sample_stats=sample_stats)
-        self.priors_name = var_names
-        return self.trace
-
-    # Diagnostic with ARVIZ ----------------------------------------------------------------------------
-    def summary(self, round_to=2, kind="stats", hdi_prob=0.89, *args, **kwargs): 
-        if self.trace is None:
-            self.trace_to_az()
-        self.tab_summary = az.summary(self.trace , round_to=round_to, kind=kind, hdi_prob=hdi_prob, *args, **kwargs)
-        return self.tab_summary 
-   
-    def diag_prior_dist(self, N = 100):
-        samples = self.sample.sample(N)
-        prob = self.log_prob(samples)
-        post_df = self.model_output_to_df(samples)
-
-        fig, axs = plt.subplots(ncols=post_df.shape[1])
-        for a in range(post_df.shape[1]-1): 
-                sns.histplot(post_df.iloc[:,a], 
-                     kde=True, stat="density",
-                     edgecolor=(1, 1, 1, .4), 
-                     ax=axs[a]).set_title(post_df.columns[a]) 
-
-        sns.histplot(list(prob.numpy()), kde=True, stat="density",
-                     edgecolor=(1, 1, 1, .4)).set_title("${\\rm logit}$")
-        self.plot_priors = fig
-        return fig
-
-    def diag_posterior(self):
-        if self.trace is None:
-            self.trace_to_az()        
-        posterior, axes = plt.subplots(1, len(self.priors_name), figsize=(8, 4))
-        axes = az.plot_posterior(self.trace , var_names=self.priors_name, ax=axes)
-        axes.flatten()[0].get_figure() 
-        self.plot_posterior = posterior
-
-    def diag_autocor(self, *args, **kwargs):
-        if self.trace is None:
-            self.trace_to_az()
-        self.autocor = az.plot_autocorr(self.trace , var_names=self.priors_name, *args, **kwargs)
-
-    def diag_traces(self, *args, **kwargs):
-        if self.trace is None:
-            self.trace_to_az()
-        self.traces =  az.plot_trace(self.trace, compact=False, *args, **kwargs)
-
-    def diag_rank(self, *args, **kwargs):
-        if self.trace is None:
-            self.trace_to_az()
-        rank, axes = plt.subplots(1, len( self.priors_name))
-        az.plot_rank(self.trace , var_names= self.priors_name, ax=axes, *args, **kwargs)
-        self.rank = rank
+    # Diag samplers --------------------------
+    class diag(diag):
+        pass
     
-    def diag_forest(self, list = None, kind = "ridgeplot", ess = True, var_names = None, *args, **kwargs):
-        if var_names is None:
-            var_names = self.priors_name
-        if list is None:
-            list = self.trace
-        self.forest = az.plot_forest(list, var_names = var_names,  kind = kind, ess = ess, *args, **kwargs)
-        return self.forest
-    
-    def diag_waic(self, *args, **kwargs):
-        if self.trace is None:
-            self.trace_to_az()        
-        self.waic = az.waic(self.trace, *args, **kwargs)
-        return self.waic
-    
-    def diag_compare(self, dict, *args, **kwargs):
-        self.comparaison = az.compare(dict, *args, **kwargs)
-        return self.comparaison 
-
-    def diag_rhat(self, *args, **kwargs):
-        if self.trace is None:
-            self.trace_to_az()        
-        self.rhat = az.rhat(self.trace, *args, **kwargs)
-        return self.rhat 
-
-    def  diag_ess(self, *args, **kwargs):
-        if self.trace is None:
-            self.trace_to_az()        
-        self.ess = az.ess(self.trace, *args, **kwargs)
-        return self.ess 
-
-    def diag_pair(self, var_names = None, 
-                  kind=["scatter", "kde"],
-                  kde_kwargs={"fill_last": False},
-                  marginals=True,
-                  point_estimate="median",
-                  figsize=(11.5, 5),
-                  *args, **kwargs):
-        if var_names is None:
-            var_names = self.priors_name
-        if self.trace is None:
-            self.trace_to_az()            
-        self.pair_plot = az.plot_pair(self.trace, var_names = var_names,                   
-                                      kind=kind,
-                                      kde_kwargs=kde_kwargs,
-                                      marginals=marginals,
-                                      point_estimate=point_estimate,
-                                      figsize=figsize,
-                                      *args, **kwargs)
-        return self.pair_plot   
-    
-    def diag_density(self, var_names=None, shade=0.2, *args, **kwargs):
-        if var_names is None:
-            var_names = self.priors_name
-        if self.trace is None:
-            self.trace_to_az()
-        self.density = az.plot_density(
-                            self.trace,
-                            var_names=var_names,
-                            shade=shade,
-                            *args, **kwargs
-                        )
-        return self.density
-    
-    def diag_plot_ess(self,):
-        if self.trace is None:
-            self.trace_to_az()
-
-        self.ess_plot = az.plot_ess(self.trace, var_names=self.priors_name, kind="evolution")
-        return self.ess_plot
-    
-    def model_checks(self):
-        params = self.priors_name
-        posterior = self.hmc_posterior
-        trace = self.trace 
-
-        posterior, axes = plt.subplots(1, len(params), figsize=(8, 4))
-        axes = az.plot_posterior(trace, var_names=params, ax=axes)
-        axes.flatten()[0].get_figure() 
-
-        autocor = az.plot_autocorr(trace, var_names=params)
-
-        traces = az.plot_trace(trace, compact=False)
-
-        rank, axes = plt.subplots(1, len(params))
-        az.plot_rank(trace, var_names=params, ax=axes)
-
-        forest = az.plot_forest(trace, var_names = params)
-
-        #summary = az.summary(trace, round_to=2, kind="stats", hdi_prob=0.89)
-
-        self.plot_posterior = posterior
-        self.autocor = autocor
-        self.traces = traces
-        self.rank = rank
-        self.forest = forest
 
 from jax.experimental import mesh_utils
 from jax.sharding import PositionalSharding
