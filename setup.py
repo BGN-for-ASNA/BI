d53d885cdad0e26a73a2049c1f5e44cb2b75acf6
SebastianSosa <s.sosa@live.fr>
2025-02-17 10:38:58 +0100

    nbda model with for loop

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+import setuptools 
 
 VERSION = '0.0.1' 
 DESCRIPTION = 'Bayesian Inferences'
 LONG_DESCRIPTION = 'Run Bayesian inferences models'
 
 # Setting up
-setup(
+setuptools.setup(
        # the name must match the folder name 'verysimplemodule'
         name="BI", 
         version=VERSION,
@@ -13,7 +13,7 @@ setup(
         author_email="<s.sosa@live.fr>",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
+        packages=setuptools.find_packages(),
         install_requires=[], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         keywords=['python', 'Bayesian inferences'],
