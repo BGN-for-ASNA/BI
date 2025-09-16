6ff0007e13cf65e265f584827b0eab0e4937fcf6
Sosa <s.sosa@live.fr>
2025-09-16 18:54:10 +0200

    PCA pipeline over (legend issues)

---
+++ Documentation/_setup.py
@@ -0,0 +1,13 @@
+# setup.py
+from tqdm.auto import tqdm as tqdm_auto
+import numpyro.util
+
+class LastOnlyTQDM(tqdm_auto):
+    def display(self, msg=None, pos=None):
+        # Only display once the bar reaches 100%
+        if self.n != self.total:
+            return
+        super().display(msg, pos)
+
+# Patch NumPyro's tqdm globally
+numpyro.util.tqdm = LastOnlyTQDM
