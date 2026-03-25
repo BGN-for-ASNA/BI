8d710408e5773d78b893ffdc1799690fdb71f083
Sosa <s.sosa@live.fr>
2026-03-25 11:05:16 +0100

    Restaured data sets

--- Test/Survival analysis/Survival analysis.py
+++ Test/Survival analysis/Survival analysis.py
@@ -1,1 +1,12 @@
-from BI import bi
+#%%
+from BI import bi,jnp
+
+m = bi(platform='cpu')
+data_path = m.load.mastectomy(only_path = True)
+m.data(data_path)
+m.df.metastasized = (m.df.metastasized == "yes").astype(np.int64)
+# %%
+m.df.event = jnp.array(m.df.event.values, dtype=jnp.int32)
+m.models.survival.surv_object(time='time', event='event', cov='metastasized', interval_length=1) # if interval = 3 we get the same as in PyMC notebook
+m.data_on_model['metastasized']
+# %%
