c836aeba57e999a688e6bf1583ef26eb914457f4
Sosa <s.sosa@live.fr>
2026-05-06 09:01:29 +0200

    density plots + log.txt for all test

--- Test/Phylogenic analysis/Model_1_Simple/compare_simple.R
+++ Test/Phylogenic analysis/Model_1_Simple/compare_simple.R
@@ -1,97 +1,75 @@
 # %%
 library(brms)
 library(ape)
-library(reticulate)
-library(BayesianInference)
-library(jsonlite)
 
-# 1. Load data and brms results
-phylo <- read.nexus("phylo.nex")
+# 1. Load data and fit brms
+phylo      <- read.nexus("phylo.nex")
 data_simple <- read.table("data_simple.txt", header = TRUE)
 A <- vcv.phylo(phylo)
-# De-compose A for BI
-L <- t(chol(A))
 
-# Load brms model for comparison
-if (file.exists("model_simple.rds")) {
-  model_simple <- readRDS("model_simple.rds")
-} else {
-  stop("brms model results not found.")
-}
+cat("\nFitting brms Simple model...\n")
+model_simple <- brm(
+  phen ~ cofactor + (1 | gr(phylo, cov = A)),
+  data = data_simple, family = gaussian(),
+  data2 = list(A = A),
+  prior = c(
+    prior(normal(0, 50), "Intercept"),
+    prior(normal(0, 10), "b"),
+    prior(student_t(3, 0, 20), "sd"),
+    prior(student_t(3, 0, 20), "sigma")
+  ),
+  chains = 2, cores = 2, iter = 3000, warmup = 1000,
+  refresh = 0
+)
 
-# 2. Check BI results
-if (file.exists("bi_post_simple.csv")) {
-  bi_post <- read.csv("bi_post_simple.csv")
+# 2. Load BI posteriors
+bi_post   <- read.csv("bi_post_simple.csv")
+post_brms <- as.data.frame(model_simple)
 
-  # 3. Compare Results
-  cat("\n--- Comparison of Posterior Means ---\n")
-  brms_sum <- summary(model_simple)
-  brms_fixef <- brms_sum$fixed
-  brms_sd_phylo <- brms_sum$random$phylo["sd(Intercept)", "Estimate"]
-  brms_sigma <- brms_sum$spec_pars["sigma", "Estimate"]
-
-  # BI summaries
-  bi_intercept_mean <- mean(bi_post$b_Intercept)
-  bi_cofactor_mean <- mean(bi_post$b_cofactor)
-  bi_sd_phylo_mean <- mean(bi_post$sd_phylo)
-  bi_sigma_mean <- mean(bi_post$sigma)
-
-  # Create comparison table
-  comparison <- data.frame(
-    Parameter = c("Intercept (uncentered)", "cofactor", "sd_phylo", "sigma"),
-    brms_Mean = c(
-      brms_fixef["Intercept", "Estimate"],
-      brms_fixef["cofactor", "Estimate"],
-      brms_sd_phylo,
-      brms_sigma
-    ),
-    BI_Mean = c(
-      bi_intercept_mean,
-      bi_cofactor_mean,
-      bi_sd_phylo_mean,
-      bi_sigma_mean
-    )
+# 3. Compare posterior means -> log.txt
+cat("\n--- Comparison of Posterior Means ---\n")
+comparison <- data.frame(
+  Parameter = c("Intercept (uncentered)", "cofactor", "sd_phylo", "sigma"),
+  brms_Mean = c(
+    mean(post_brms$b_Intercept),
+    mean(post_brms$b_cofactor),
+    mean(post_brms$sd_phylo__Intercept),
+    mean(post_brms$sigma)
+  ),
+  BI_Mean = c(
+    mean(bi_post$b_Intercept),
+    mean(bi_post$b_cofactor),
+    mean(bi_post$sd_phylo),
+    mean(bi_post$sigma)
   )
+)
+comparison$Difference <- comparison$BI_Mean - comparison$brms_Mean
+print(comparison)
+write.csv(comparison, "comparison_simple.csv", row.names = FALSE)
+cat("\n--- SIMPLE MODEL COMPARISON ---\n", file = "log.txt", append = TRUE)
+write.table(comparison, file = "log.txt", append = TRUE,
+            row.names = FALSE, sep = "\t", quote = FALSE)
 
-  print(comparison)
-  write.csv(comparison, "comparison_simple.csv", row.names = FALSE)
-
-  # Save to log.txt
-  cat("\n--- SIMPLE MODEL COMPARISON ---\n", file = "log.txt", append = TRUE)
-  write.table(comparison, file = "log.txt", append = TRUE, row.names = FALSE, sep = "\t", quote = FALSE)
-
-  # 4. Density Plots
-  cat("\nGenerating density plots...\n")
-  dir.create("plots", showWarnings = FALSE)
-
-  # Extract draws
-  post_brms <- as.data.frame(model_simple)
-
-  plot_density <- function(param_name, brms_samples, bi_samples, file_name) {
-    svg(paste0("plots/", file_name, ".svg"), width = 6, height = 4)
-    brms_samples <- as.numeric(as.vector(brms_samples))
-    bi_samples <- as.numeric(bi_samples)
-
-    d_brms <- density(brms_samples)
-    d_bi <- density(bi_samples)
-
-    xlim <- range(c(d_brms$x, d_bi$x))
-    ylim <- range(c(d_brms$y, d_bi$y))
-
-    plot(d_brms,
-      col = "red", lwd = 2, main = paste("Density Comparison:", param_name),
-      xlim = xlim, ylim = ylim, xlab = "Value", ylab = "Density"
-    )
-    lines(d_bi, col = "blue", lwd = 2, lty = 2)
-    legend("topright", legend = c("brms", "BI"), col = c("red", "blue"), lwd = 2, lty = c(1, 2))
-    dev.off()
-  }
-
-  plot_density("Intercept (uncentered)", post_brms$b_Intercept, bi_post$b_Intercept, "simple_intercept")
-  plot_density("cofactor", post_brms$b_cofactor, bi_post$b_cofactor, "simple_cofactor")
-  plot_density("sd_phylo", post_brms$sd_phylo__Intercept, bi_post$sd_phylo, "simple_sd_phylo")
-  plot_density("sigma", post_brms$sigma, bi_post$sigma, "simple_sigma")
-} else {
-  cat("BI posterior file not found. Run fit_bi_simple.py first.\n")
+# 4. Combined density panel (all parameters)
+cat("\nGenerating combined density comparison panel...\n")
+draw_density <- function(label, brms_s, bi_s) {
+  brms_s <- as.numeric(na.omit(as.vector(brms_s)))
+  bi_s   <- as.numeric(na.omit(bi_s))
+  d1 <- density(brms_s); d2 <- density(bi_s)
+  xlim <- range(c(d1$x, d2$x)); ylim <- range(c(d1$y, d2$y))
+  plot(d1, col = "red", lwd = 2, main = label,
+       xlim = xlim, ylim = ylim, xlab = "Value", ylab = "Density")
+  lines(d2, col = "blue", lwd = 2, lty = 2)
+  legend("topright", legend = c("brms", "BI"),
+         col = c("red", "blue"), lwd = 2, lty = c(1, 2), cex = 0.8)
 }
+
+png("density_comparison.png", width = 1200, height = 800, res = 120)
+par(mfrow = c(2, 2), mar = c(4, 4, 3, 1))
+draw_density("Intercept (uncentered)", post_brms$b_Intercept,         bi_post$b_Intercept)
+draw_density("cofactor",               post_brms$b_cofactor,          bi_post$b_cofactor)
+draw_density("sd_phylo",               post_brms$sd_phylo__Intercept, bi_post$sd_phylo)
+draw_density("sigma",                  post_brms$sigma,               bi_post$sigma)
+dev.off()
+cat("Panel saved to density_comparison.png\n")
 # %%
--- Test/Phylogenic analysis/Model_3_Repeated/compare_repeat.R
+++ Test/Phylogenic analysis/Model_3_Repeated/compare_repeat.R
@@ -1,24 +1,22 @@
 # %%
 library(brms)
 library(ape)
-library(reticulate)
 
 # 1. Load data and fit brms
-phylo <- read.nexus("phylo.nex")
+phylo       <- read.nexus("phylo.nex")
 data_repeat <- read.table("data_repeat.txt", header = TRUE)
-data_repeat$spec_mean_cf <- with(data_repeat, tapply(cofactor, species, mean)[species])
+data_repeat$spec_mean_cf   <- with(data_repeat, tapply(cofactor, species, mean)[species])
 data_repeat$within_spec_cf <- data_repeat$cofactor - data_repeat$spec_mean_cf
-
 A <- vcv.phylo(phylo)
 
-cat("\nFitting brms model...\n")
+cat("\nFitting brms Repeated model...\n")
 model_repeat <- brm(
   phen ~ spec_mean_cf + within_spec_cf + (1 | gr(phylo, cov = A)) + (1 | species),
   data = data_repeat, family = gaussian(),
   data2 = list(A = A),
   prior = c(
-    prior(normal(0, 10), "b"),
     prior(normal(0, 50), "Intercept"),
+    prior(normal(0, 10), "b"),
     prior(student_t(3, 0, 20), "sd"),
     prior(student_t(3, 0, 20), "sigma")
   ),
@@ -26,73 +24,60 @@ model_repeat <- brm(
   refresh = 0
 )
 
-# 2. Load BI results
-bi_post <- read.csv("bi_post_repeat.csv")
+# 2. Load BI posteriors
+bi_post   <- read.csv("bi_post_repeat.csv")
+post_brms <- as.data.frame(model_repeat)
 
-# 3. Compare Results
+# 3. Compare posterior means -> log.txt
 cat("\n--- Comparison of Posterior Means ---\n")
-brms_sum <- summary(model_repeat)
-brms_fixef <- brms_sum$fixed
-brms_sd_phylo <- brms_sum$random$phylo["sd(Intercept)", "Estimate"]
-brms_sd_spec <- brms_sum$random$species["sd(Intercept)", "Estimate"]
-brms_sigma <- brms_sum$spec_pars["sigma", "Estimate"]
-
 comparison <- data.frame(
   Parameter = c("Intercept (uncentered)", "spec_mean_cf", "within_spec_cf", "sd_phylo", "sd_species", "sigma"),
   brms_Mean = c(
-    brms_fixef["Intercept", "Estimate"],
-    brms_fixef["spec_mean_cf", "Estimate"],
-    brms_sd_phylo,
-    brms_sd_spec,
-    brms_fixef["within_spec_cf", "Estimate"],
-    brms_sigma
+    mean(post_brms$b_Intercept),
+    mean(post_brms$b_spec_mean_cf),
+    mean(post_brms$b_within_spec_cf),
+    mean(post_brms$sd_phylo__Intercept),
+    mean(post_brms$sd_species__Intercept),
+    mean(post_brms$sigma)
   ),
   BI_Mean = c(
     mean(bi_post$b_Intercept),
     mean(bi_post$b_spec_mean_cf),
+    mean(bi_post$b_within_spec_cf),
     mean(bi_post$sd_phylo),
     mean(bi_post$sd_species),
-    mean(bi_post$b_within_spec_cf),
     mean(bi_post$sigma)
   )
 )
-
+comparison$Difference <- comparison$BI_Mean - comparison$brms_Mean
 print(comparison)
 write.csv(comparison, "comparison_repeat.csv", row.names = FALSE)
-
-# Save to log.txt
 cat("\n--- REPEAT MODEL COMPARISON ---\n", file = "log.txt", append = TRUE)
-write.table(comparison, file = "log.txt", append = TRUE, row.names = FALSE, sep = "\t", quote = FALSE)
+write.table(comparison, file = "log.txt", append = TRUE,
+            row.names = FALSE, sep = "\t", quote = FALSE)
 
-# 4. Density Plots
-cat("\nGenerating density plots...\n")
-dir.create("plots", showWarnings = FALSE)
-
-post_brms <- as.data.frame(model_repeat)
-
-plot_density <- function(param_name, brms_samples, bi_samples, file_name) {
-  svg(paste0("plots/", file_name, ".svg"), width = 6, height = 4)
-  brms_samples <- as.numeric(as.vector(brms_samples))
-  bi_samples <- as.numeric(bi_samples)
-
-  d_brms <- density(brms_samples)
-  d_bi <- density(bi_samples)
-
-  xlim <- range(c(d_brms$x, d_bi$x))
-  ylim <- range(c(d_brms$y, d_bi$y))
-
-  plot(d_brms,
-    col = "red", lwd = 2, main = paste("Density Comparison:", param_name),
-    xlim = xlim, ylim = ylim, xlab = "Value", ylab = "Density"
-  )
-  lines(d_bi, col = "blue", lwd = 2, lty = 2)
-  legend("topright", legend = c("brms", "BI"), col = c("red", "blue"), lwd = 2, lty = c(1, 2))
-  dev.off()
+# 4. Combined density panel (all parameters)
+cat("\nGenerating combined density comparison panel...\n")
+draw_density <- function(label, brms_s, bi_s) {
+  brms_s <- as.numeric(na.omit(as.vector(brms_s)))
+  bi_s   <- as.numeric(na.omit(bi_s))
+  d1 <- density(brms_s); d2 <- density(bi_s)
+  xlim <- range(c(d1$x, d2$x)); ylim <- range(c(d1$y, d2$y))
+  plot(d1, col = "red", lwd = 2, main = label,
+       xlim = xlim, ylim = ylim, xlab = "Value", ylab = "Density")
+  lines(d2, col = "blue", lwd = 2, lty = 2)
+  legend("topright", legend = c("brms", "BI"),
+         col = c("red", "blue"), lwd = 2, lty = c(1, 2), cex = 0.8)
 }
 
-plot_density("Intercept (uncentered)", post_brms$b_Intercept, bi_post$b_Intercept, "repeat_intercept")
-plot_density("spec_mean_cf", post_brms$b_spec_mean_cf, bi_post$b_spec_mean_cf, "repeat_spec_mean_cf")
-plot_density("within_spec_cf", post_brms$b_within_spec_cf, bi_post$b_within_spec_cf, "repeat_within_spec_cf")
-plot_density("sd_phylo", post_brms$sd_phylo__Intercept, bi_post$sd_phylo, "repeat_sd_phylo")
-plot_density("sd_species", post_brms$sd_species__Intercept, bi_post$sd_species, "repeat_sd_species")
-plot_density("sigma", post_brms$sigma, bi_post$sigma, "repeat_sigma")
+png("density_comparison.png", width = 1800, height = 800, res = 120)
+par(mfrow = c(2, 3), mar = c(4, 4, 3, 1))
+draw_density("Intercept (uncentered)", post_brms$b_Intercept,           bi_post$b_Intercept)
+draw_density("spec_mean_cf",           post_brms$b_spec_mean_cf,        bi_post$b_spec_mean_cf)
+draw_density("within_spec_cf",         post_brms$b_within_spec_cf,      bi_post$b_within_spec_cf)
+draw_density("sd_phylo",               post_brms$sd_phylo__Intercept,   bi_post$sd_phylo)
+draw_density("sd_species",             post_brms$sd_species__Intercept, bi_post$sd_species)
+draw_density("sigma",                  post_brms$sigma,                 bi_post$sigma)
+dev.off()
+cat("Panel saved to density_comparison.png\n")
+# %%
